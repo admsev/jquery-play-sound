08dafa67b62db191f3722531f8e4a62bfb9a6938
Alexander Manzyuk <admsev@gmail.com>
2012-12-19 19:23:45 +0200

    No more #playSound

--- jquery.playSound.js
+++ jquery.playSound.js
@@ -3,16 +3,13 @@
  * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
  * https://github.com/admsev/jquery-play-sound
  * Usage: $.playSound('http://example.org/sound.mp3');
-*/
+**/
 
 (function($){
-  $(document).ready(function() {
-      $('body').append('<span id="playSound"></span>');
-  });
 
   $.extend({
     playSound: function(){
-      return $("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false'>").appendTo('#playSound');
+      return $("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false' class='playSound'>").appendTo('body');
     }
   });
 
