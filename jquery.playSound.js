b1cd0b2b58c12600b2d8ea683e7a74e9994f840a
Kirill Zabarniuk <kirill.zabarniuk@gmail.com>
2015-12-11 16:55:39 +0200

    Format code

--- jquery.playSound.js
+++ jquery.playSound.js
@@ -9,7 +9,13 @@
 
   $.extend({
     playSound: function(){
-      return $("<embed src='"+arguments[0]+".mp3' hidden='true' autostart='true' loop='false' class='playSound'>" + "<audio autoplay='autoplay' style='display:none;' controls='controls'><source src='"+arguments[0]+".mp3' /><source src='"+arguments[0]+".ogg' /></audio>").appendTo('body');
+      return $(
+        '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
+        + '<audio autoplay="autoplay" style="display:none;" controls="controls">'
+          + '<source src="' + arguments[0] + '.mp3" />'
+          + '<source src="' + arguments[0] + '.ogg" />'
+        + '</audio>'
+      ).appendTo('body');
     }
   });
 
