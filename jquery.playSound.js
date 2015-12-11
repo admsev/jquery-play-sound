8159c920b1ec75cd367ef78b01a20385d0d65df8
Kirill Zabarniuk <kirill.zabarniuk@gmail.com>
2015-12-11 16:57:23 +0200

    Play once

--- jquery.playSound.js
+++ jquery.playSound.js
@@ -10,10 +10,10 @@
   $.extend({
     playSound: function(){
       return $(
-        '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
-        + '<audio autoplay="autoplay" style="display:none;">'
+        '<audio autoplay="autoplay" style="display:none;">'
           + '<source src="' + arguments[0] + '.mp3" />'
           + '<source src="' + arguments[0] + '.ogg" />'
+          + '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
         + '</audio>'
       ).appendTo('body');
     }
