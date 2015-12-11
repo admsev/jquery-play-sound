5075c9444cc550a9a349fe0a20a8cae8cf7274bd
Kirill Zabarniuk <kirill.zabarniuk@gmail.com>
2015-12-11 16:56:16 +0200

    Controls not used

--- jquery.playSound.js
+++ jquery.playSound.js
@@ -11,7 +11,7 @@
     playSound: function(){
       return $(
         '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
-        + '<audio autoplay="autoplay" style="display:none;" controls="controls">'
+        + '<audio autoplay="autoplay" style="display:none;">'
           + '<source src="' + arguments[0] + '.mp3" />'
           + '<source src="' + arguments[0] + '.ogg" />'
         + '</audio>'
