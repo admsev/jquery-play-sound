866a1e9a08d59f2feb1794d8cd1fa1f543374a07
Dongdong Yang <hi@dongdong.ca>
2017-01-26 09:05:03 -0500

    More convinent
    play all audio types supported by IE9+, Chrome, Edge....

--- jquery.playSound.js
+++ jquery.playSound.js
@@ -2,21 +2,25 @@
  * @author Alexander Manzyuk <admsev@gmail.com>
  * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
  * https://github.com/admsev/jquery-play-sound
- * Usage: $.playSound('http://example.org/sound');
+ * Usage: $.playSound('http://example.org/sound')
+ * $.playSound('http://example.org/sound.wav')
+ * $.playSound('/attachments/sounds/1234.wav')
+ * $.playSound('/attachments/sounds/1234.mp3')
+ * $.stopSound();
 **/
 
-(function($){
-
-  $.extend({
-    playSound: function(){
-      return $(
-        '<audio autoplay="autoplay" style="display:none;">'
-          + '<source src="' + arguments[0] + '.mp3" />'
-          + '<source src="' + arguments[0] + '.ogg" />'
-          + '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
-        + '</audio>'
-      ).appendTo('body');
-    }
-  });
-
+(function ($) {
+    $.extend({
+        playSound: function () {
+            return $(
+                   '<audio class="sound-player" autoplay="autoplay" style="display:none;">'
+                     + '<source src="' + arguments[0] + '" />'
+                     + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+                   + '</audio>'
+                 ).appendTo('body');
+        },
+        stopSound: function () {
+            $(".sound-player").remove();
+        }
+    });
 })(jQuery);
